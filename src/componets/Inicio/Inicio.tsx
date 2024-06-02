17caf188556557bf103d49df0e4a21ece3c48e57
Vinicius <viniciusmsantos02@gmail.com>
2024-06-02 00:06:01 -0300

    estruturação e estlização do hero

--- src/componets/Inicio/Inicio.tsx
+++ src/componets/Inicio/Inicio.tsx
@@ -4,7 +4,8 @@ export default function Inicio() {
     return (
         <>
             <section className={styles.inicio}>
-                <h1> SOS Oceanos </h1>
+                <h1 className={styles.titulo}> S.O.S OCEANOS </h1>
+                <p className={styles.subtitulo}> Nossos oceanos, vastos e misteriosos, estão clamando por ajuda. A poluição marinha é uma ameaça invisível, mas devastadora, que está destruindo ecossistemas inteiros e colocando em risco a vida marinha. O tempo para agir é agora. Cada segundo conta. </p>
             </section>
         </>
     )
