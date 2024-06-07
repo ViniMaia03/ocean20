f92248cc12aca8acf5a74e6688ce6f13060d17a0
Vinicius <viniciusmsantos02@gmail.com>
2024-06-07 00:21:22 -0300

    alteracao do README.md

--- README.md
+++ README.md
@@ -1,36 +1,16 @@
-This is a [Next.js](https://nextjs.org/) project bootstrapped with [`create-next-app`](https://github.com/vercel/next.js/tree/canary/packages/create-next-app).
+# INTRODUÇÃO:
 
-## Getting Started
+A poluição marinha é um dos maiores desafios ambientais enfrentados pelo mundo contemporâneo, afetando diretamente a saúde dos ecossistemas oceânicos e das comunidades humanas que dependem deles. Esta forma de poluição envolve a introdução de substâncias nocivas e resíduos no ambiente marinho, provenientes de diversas fontes, como esgoto não tratado, resíduos plásticos, produtos químicos industriais, derramamentos de óleo, entre outros. Esses poluentes têm impactos devastadores sobre a vida marinha, desde a menor das criaturas planctônicas até os grandes mamíferos e aves marinhas. Além de ameaçar a biodiversidade, a poluição marinha compromete a qualidade da água, a pesca, o turismo e, em última instância, a saúde humana. Compreender a extensão e as consequências dessa poluição é fundamental para desenvolver estratégias eficazes de mitigação e preservação dos oceanos, essenciais para o equilíbrio ambiental e a sustentabilidade do planeta.
 
-First, run the development server:
+---
 
-```bash
-npm run dev
-# or
-yarn dev
-# or
-pnpm dev
-# or
-bun dev
-```
+## SOLUÇÃO:
 
-Open [http://localhost:3000](http://localhost:3000) with your browser to see the result.
+Criação de um portal para conscientização sobre a poluição marinha, juntamente com o compartilhamento de algumas ONG's que cuidam dos oceanos de uma forma geral. Criamos um portal em Next.js para solução do tema proposto.
 
-You can start editing the page by modifying `app/page.tsx`. The page auto-updates as you edit the file.
+---
 
-This project uses [`next/font`](https://nextjs.org/docs/basic-features/font-optimization) to automatically optimize and load Inter, a custom Google Font.
+## ACESSOS:
 
-## Learn More
-
-To learn more about Next.js, take a look at the following resources:
-
-- [Next.js Documentation](https://nextjs.org/docs) - learn about Next.js features and API.
-- [Learn Next.js](https://nextjs.org/learn) - an interactive Next.js tutorial.
-
-You can check out [the Next.js GitHub repository](https://github.com/vercel/next.js/) - your feedback and contributions are welcome!
-
-## Deploy on Vercel
-
-The easiest way to deploy your Next.js app is to use the [Vercel Platform](https://vercel.com/new?utm_medium=default-template&filter=next.js&utm_source=create-next-app&utm_campaign=create-next-app-readme) from the creators of Next.js.
-
-Check out our [Next.js deployment documentation](https://nextjs.org/docs/deployment) for more details.
+link do site: https://ocean20.vercel.app/
+link do repositório: https://github.com/ViniMaia03/ocean20
--- src/componets/Oceanos/oceanos.tsx
+++ src/componets/Oceanos/oceanos.tsx
@@ -13,11 +13,11 @@ export default function Oceanos() {
 
     const oceanosInfo = [
         { value: '', label: '', img: '', info: '' },
-        { value: 'atlantico', label: 'Atlântico', img: '/assets/oceano-atlantico.jpg', info: 'Informações sobre o Atlântico' },
-        { value: 'pacifico', label: 'Pacífico', img: '/assets/oceano-atlantico.jpg', info: 'Informações sobre o Pacífico' },
-        { value: 'indico', label: 'Índico', img: '/assets/oceano-atlantico.jpg', info: 'Informações sobre o Índico' },
-        { value: 'artico', label: 'Ártico', img: '/assets/oceano-atlantico.jpg', info: 'Informações sobre o Ártico' },
-        { value: 'antartico', label: 'Antártico', img: '/assets/oceano-atlantico.jpg', info: 'Informações sobre o Antártico' },
+        { value: 'atlantico', label: 'Atlântico', img: '/assets/oceano-atlantico.jpg', info: 'O Oceano Atlântico é o segundo maior oceano do mundo, cobrindo aproximadamente 106 milhões de quilômetros quadrados. Ele separa as Américas da Europa e da África, e é conhecido por sua importância histórica nas explorações e comércio. O Atlântico possui diversas correntes oceânicas, como a Corrente do Golfo, que influenciam os climas dos continentes adjacentes. É um oceano rico em biodiversidade, abrigando uma vasta gama de ecossistemas marinhos. Além disso, é uma via crucial para o transporte marítimo e possui recursos naturais significativos, incluindo pesca e reservas de petróleo.' },
+        { value: 'pacifico', label: 'Pacífico', img: '/assets/oceano-atlantico.jpg', info: 'O Oceano Pacífico é o maior e mais profundo oceano do mundo, cobrindo cerca de 168 milhões de quilômetros quadrados. Ele se estende desde a América do Norte e do Sul até a Ásia e a Austrália, e é conhecido pela sua vasta extensão e profundidade, incluindo a Fossa das Marianas, o ponto mais profundo da Terra. O Pacífico abriga uma enorme diversidade de vida marinha e muitos ecossistemas únicos, como os recifes de coral. Suas correntes oceânicas, como a Corrente de Humboldt, desempenham um papel crucial no clima global. Além de ser uma rota vital para o comércio internacional, o Pacífico é rico em recursos naturais, incluindo pesca abundante e reservas de minerais e petróleo no fundo do mar.' },
+        { value: 'indico', label: 'Índico', img: '/assets/oceano-atlantico.jpg', info: 'O Oceano Índico é o terceiro maior oceano do mundo, cobrindo cerca de 70 milhões de quilômetros quadrados. Ele está localizado entre a África, a Ásia, a Austrália e a Antártica, sendo conhecido por suas águas quentes e ricas em biodiversidade. O Índico desempenha um papel crucial no clima regional, influenciando os padrões das monções que afetam grande parte do sul da Ásia. Suas rotas marítimas são essenciais para o comércio global, especialmente para o transporte de petróleo e mercadorias entre o Oriente Médio, África e Ásia. Além disso, o oceano é uma importante fonte de recursos naturais, incluindo áreas ricas em pesca e depósitos de petróleo e gás natural.' },
+        { value: 'artico', label: 'Ártico', img: '/assets/oceano-atlantico.jpg', info: 'O Oceano Ártico é o menor e mais raso dos oceanos do mundo, cobrindo cerca de 14 milhões de quilômetros quadrados. Localizado ao redor do Polo Norte, ele é caracterizado por suas condições extremas, com grande parte de sua superfície coberta por gelo marinho durante a maior parte do ano. O Ártico é crucial para a regulação do clima global, refletindo a radiação solar e influenciando os padrões climáticos. Este oceano abriga uma fauna única, incluindo ursos polares, focas e baleias. Com o derretimento do gelo devido às mudanças climáticas, o Ártico está se tornando uma nova fronteira para a navegação e exploração de recursos, incluindo petróleo e gás, apesar dos desafios ambientais e geopolíticos.' },
+        { value: 'antartico', label: 'Antártico', img: '/assets/oceano-atlantico.jpg', info: 'O Oceano Antártico, também conhecido como Oceano Austral, circunda o continente da Antártica e cobre aproximadamente 20 milhões de quilômetros quadrados. É o quarto maior oceano do mundo e se distingue por suas águas frias e turbulentas, alimentadas pela Corrente Circumpolar Antártica, a corrente oceânica mais forte do planeta. O Oceano Antártico desempenha um papel vital na regulação do clima global e no ciclo de nutrientes dos oceanos. Suas águas abrigam uma rica biodiversidade, incluindo krill, focas, pinguins e baleias. Além disso, este oceano é fundamental para a pesquisa científica sobre mudanças climáticas, pois a região é um indicador sensível das transformações ambientais globais.' },
     ];
 
     return(
@@ -35,9 +35,8 @@ export default function Oceanos() {
                     ))}
                 </select>
 
-                <div>
-                    <Image src={selecionado.img} alt={selecionado.label} width={300} height={300} className={styles.img_oceanos} />
-                    <p>{selecionado.info}</p>
+                <div className={styles.resultado}>
+                    <p className={styles.desc}>{selecionado.info}</p>
                 </div>
             </section>
         </>
